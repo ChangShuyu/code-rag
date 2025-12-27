f2969418313428bfc7dca442efa67a3e004d75f4
ChangShuyu <csy1999@qq.com>
2025-12-27 17:30:22 +0800

    add mbpp shell script

---
+++ run_mbpp.sh
@@ -0,0 +1,19 @@
+PYTHONPATH=./ python -m create.mbpp
+
+python eval_beir_sbert_canonical.py \
+    --model "BAAI/bge-base-en-v1.5" \
+    --dataset mbpp \
+    --batch_size 64 \
+    --output_file results/mbpp_bge_scores.json \
+    --results_file results/mbpp_bge_retrieval.json
+
+cd ../generation/
+python main.py \
+    --task "mbpp" \
+    --model "bigcode/starcoder2-3b" \
+    --model_backend vllm \
+    --dataset_path "mbpp" \
+    --allow_code_execution \
+    --save_generations \
+    --save_generations_path "results/mbpp_baseline_generations.json" \
+    --metric_output_path "results/mbpp_baseline_results.json"
